93acf7c8ca9b760a61709cfd721e24b16ea3500a
unknown <nvt.kscntt@gmail.com>
2023-07-02 14:36:15 +0700

    upload project

---
+++ examples/submit_outlook_token.py
@@ -0,0 +1,31 @@
+from selenium import webdriver
+import re
+import requests
+from os import environ
+import threading
+import time
+from onestCaptcha import onestCaptchaClient
+import random
+
+def demo_funcaptcha():
+    APIKEY = "0aa92cd8393a49698c408ea0ee56c2a5"
+    client = onestCaptchaClient(apikey=APIKEY)
+    sitekey = "2CB16598-CB82-4CF7-B332-5990DB66F3AB"
+    siteurl = "https://twitter.com"
+    while(True):
+        result = client.funCaptcha_task_proxyless(siteurl, sitekey)
+        # result= client.image_to_text(base64img="base64img") # if you send base64img
+        if result["code"] == 0:  # success:
+            return result["token"]
+        else:  # wrong
+            continue
+driver= webdriver.Chrome(executable_path="chromedriver.exe")
+driver.get("https://outlook.com")
+# send username, password, birthday,......
+input()
+driver.switch_to.frame(driver.find_element_by_id('enforcementFrame'))
+time.sleep(0.5)
+token= demo_funcaptcha()
+print(token)
+driver.execute_script('parent.postMessage(JSON.stringify({eventId:"challenge-complete",payload:{sessionToken:"'+token+'"}}),"*")')
+input()
---
+++ setup.py
@@ -0,0 +1,3 @@
+from setuptools import setup
+
+setup(name='onestCaptcha',version='1.0',author='onestCaptcha',author_email='onestCaptcha@support.com',packages=["onestCaptcha"])
